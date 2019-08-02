84d12ddcdb2db2d8d1e4c49bae65edb6259b8ecc
x1aolata <45487873+x1aolata@users.noreply.github.com>
2019-08-02 23:08:42 +0800

    Material

--- chapter12/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
+++ chapter12/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.materialtest;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -9,24 +11,27 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar =(Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mDrawerLayout=findViewById(R.id.drawer_layout);
+
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navView = findViewById(R.id.nav_view);
         ActionBar actionBar = getSupportActionBar();
-        if(actionBar!=null)
-        {
+        if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+//            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
         navView.setCheckedItem(R.id.nav_call);
         navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -36,8 +41,28 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this,"你按了按钮！！",Toast.LENGTH_SHORT).show();
+
+                Snackbar.make(v, "删东西了", Snackbar.LENGTH_SHORT)
+                        .setAction("别啊", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this, "那就不删了", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+            }
+        });
     }
 
+
+
+    // 设置菜单
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar, menu);
         return true;
@@ -46,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case  android.R.id.home:
+            case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 break;
 
--- chapter12/README.md
+++ chapter12/README.md
@@ -3,3 +3,85 @@
 
 **浅色主题**：Theme.AppCompat.Light.NoActionBar
 **深色主题**：Theme.AppCompat.NoActionBar
+
+### Toolbar
+### DrawerLayout（抽屉式布局）
+```
+<android.support.v4.widget.DrawerLayout>
+
+    <XXXLayout>
+	主布局
+    </XXXLayout>
+
+    <XXXLayout>
+	滑动菜单
+    android:layout_gravity="start"
+    </XXXLayout>
+</android.support.v4.widget.DrawerLayout>
+```
+其中子菜单中的**layout_gravity**属性必须指定
+
+* start——表示会根据系统语言进行判断
+* left——左边
+* right——右边
+
+### NavigationView
+
+依赖：implementation 'com.android.support:design:28.0.0'
+
+#### CircleImageView——实现图片圆形化
+依赖：implementation 'de.hdodenhof:circleimageview:2.1.0'
+
+-----
+
+
+### FloatingActionButton（悬浮式按钮）
+```
+<android.support.design.widget.FloatingActionButton
+            android:id="@+id/fab"
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:layout_gravity="bottom|end"				//位置，可以设置为start
+            android:layout_margin="16dp"
+            app:backgroundTint="#000000"				//背景颜色
+            app:rippleColor="#C90E0E"					//点击颜色
+            android:src="@drawable/ic_done"				//图标
+            app:elevation="8dp" 					//悬浮高度
+            />
+```
+### SnackBar
+```
+ Snackbar.make(v, "删东西了", Snackbar.LENGTH_SHORT)
+                        .setAction("别啊", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this, "那就不删了", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+```
+
+### CoordinatorLayout
+加强版FrameLayout  
+自动调整
+
+
+-----
+
+
+##卡片式布局
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
